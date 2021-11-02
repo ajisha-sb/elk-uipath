064db7f2eb8c1129e7a2c552a946e3276f8b2f3c
ajisha-sb <ajisha.sb@vismayacorp.com>
2021-11-02 17:28:30 +0530

    first commit

--- elstcsrch.sh
+++ elstcsrch.sh
@@ -1,3 +1,3 @@
-sed -i "s|#network.host: 192.168.0.1|network.host:38.91.102.9|g" /etc/elasticsearch/elasticsearch.yml
+sed -i "s|#network.host: 192.168.0.1|network.host: 38.91.102.9|g" /etc/elasticsearch/elasticsearch.yml
 sed -i "s|#http.port: 9200|http.port: 9200|g" /etc/elasticsearch/elasticsearch.yml
 sed -i '71idiscovery.type: single-node' /etc/elasticsearch/elasticsearch.yml
